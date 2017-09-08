6a56b62b2a062c878c726efb3bbe7e3812800115
YouGoBOOM <31582062+YouGoBOOM@users.noreply.github.com>
2017-09-08 11:02:30 -0400

    8 directional movement animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     // Initializing Variables
-    public float moveSpeed = 4;     // Walking Movement Speed
-    public bool rightMouseClicked = false;
-    public Vector3 mouseWorldSpace;
-    private Animator animator;      // Animator
+    public float moveSpeed = 4f;             // Walking Movement Speed
+    public bool rightMouseClicked = false;   // Check if right mouse was clicked
+    public Vector3 mouseWorldSpace;          // Make target for right click global
+    private Animator animator;               // Animator
+    public bool playerMoving = false;        // Check if player is moving
+    public float direction;                  // Current direction
+    public float lastDirection;              // Last direction when idle
     
     // Use this for initialization
 	void Start () {
@@ -22,21 +25,67 @@ public class PlayerController : MonoBehaviour {
         // Right click movement
         OnMouseRightClick();
         if (rightMouseClicked == true && transform.position != mouseWorldSpace) {
+            // Move until at target
+            direction = CalculateDirection(mouseWorldSpace.x, mouseWorldSpace.y, transform.position.x, transform.position.y);
             transform.position = Vector3.MoveTowards(transform.position, mouseWorldSpace, moveSpeed * Time.deltaTime);
         } else if (transform.position == mouseWorldSpace) {
+            // When at target, stop moving
+            lastDirection = direction;
             rightMouseClicked = false;
+            playerMoving = false;
         }
 
+        // Set parameters in animator
+        animator.SetFloat("CurrentDirection", direction);
+        animator.SetFloat("LastDirection", lastDirection);
+        animator.SetBool("PlayerMoving", playerMoving);
     }
 
     // Right click screen to world positioning
     private void OnMouseRightClick() {
         if (Input.GetMouseButtonDown(1)) {
             rightMouseClicked = true;
+            playerMoving = true;
             Vector3 mouseScreenPosition = Input.mousePosition;
             mouseWorldSpace = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
             mouseWorldSpace.z = 0;
         }
-        
+    }
+
+    // Calculates direction of movement
+    // Returns direction at int
+    // 0 = right, 1 = up-right, 2 = up, 3 = up-left, 4 = left, 5 = down-left, 6 = down, 7 = down-right
+    private float CalculateDirection(float targetLocationX, float targetLocationY, float currentLocationX, float currentLocationY) {
+        float angle;
+        float direction;
+        float deltaX = targetLocationX - currentLocationX;
+        float deltaY = targetLocationY - currentLocationY;
+        if (targetLocationX > currentLocationX) {
+            angle = 2 * Mathf.PI + Mathf.Atan2(deltaY, deltaX);
+        } else if (targetLocationX < currentLocationX) {
+            angle = 2 * Mathf.PI + Mathf.Atan2(deltaY, deltaX);
+        } else {
+            if (deltaY > 0f) {
+                angle = Mathf.PI / 2;
+            } else {
+                angle = 3 * Mathf.PI / 2;
+            }
+        }
+        if (angle >= 2 * Mathf.PI) {
+            angle -= 2 * Mathf.PI;
+        }
+        angle /= (Mathf.PI / 4);
+        // Mathf.Round rounds 0.5 to the even number
+        // 10.5 --> 10
+        // Created own rounding function
+        if (angle % 1f >= 0.5f) {
+            angle += 0.5f;
+        }
+        direction = Mathf.Floor(angle);
+        if (direction == 8f) {
+            return 0f;
+        } else {
+            return direction;
+        }
     }
 }
