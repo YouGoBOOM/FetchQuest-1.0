5ec73a2b9bab173ff4b9de10a4981769126e3ff0
YouGoBOOM <31582062+YouGoBOOM@users.noreply.github.com>
2017-11-03 09:40:08 -0400

    npc movement can be looped or reversed

--- Assets/Scripts/NPCMovement.cs
+++ Assets/Scripts/NPCMovement.cs
@@ -10,8 +10,12 @@ public class NPCMovement : MonoBehaviour {
     public Vector3[] stops;                     // Getting an array of steps
     public float[] waitTimeDuringStops;         // Getting how much time waiting at a stop
     public float waitTimeDuringStopsCounter;    // Counter for wait time counter
-    public bool isWalking;                      // Checking if the NPC is walking
+    public bool isWalking;                      // Check if the NPC is walking
     public PlayerController playerController;   // Getting the player controller
+    public bool loopStops;                      // Check if NPC loops through stops array
+    public bool reverseStops;                   // Check if NPC reverse through stops array
+    public bool isReversing;                    // Check if NPC is reversing through stops array
+    public bool endWalkingSequence;            // Check if NPC walking sequence is ended
 
 	// Use this for initialization
 	void Start () {
@@ -44,31 +48,62 @@ public class NPCMovement : MonoBehaviour {
 
     // Move towards the stops
     private void Moving() {
-        // Check if not walking
-        if (!isWalking) {
-            // Count down counter
-            if (waitTimeDuringStopsCounter > 0) {
-                waitTimeDuringStopsCounter -= Time.deltaTime;
-            } else {
-                // Increase current stop
-                currentStop++;
-                // Loop back if at end
-                if (currentStop == stops.Length) {
-                    currentStop = 0;
+        if (!endWalkingSequence) {
+            // Check if not walking
+            if (!isWalking) {
+                // Count down counter
+                if (waitTimeDuringStopsCounter > 0) {
+                    waitTimeDuringStopsCounter -= Time.deltaTime;
+                } else {
+                    // Check if stops should reverse
+                    if (reverseStops) {
+                        // Check if going forward through stops
+                        if (!isReversing) {
+                            // Increase current stop
+                            currentStop++;
+                            // If at last stop, reverse 
+                            if (currentStop == stops.Length - 1) {
+                                isReversing = true;
+                            }
+                            // Check if going reverse through stops
+                        } else {
+                            // Decrease current stop
+                            currentStop--;
+                            // If at first stop, go forward
+                            if (currentStop == 0) {
+                                isReversing = false;
+                            }
+                        }
+                        // Check if stops should loop
+                    } else if (loopStops) {
+                        currentStop++;
+                        // Loop back if at end
+                        if (currentStop == stops.Length) {
+                            currentStop = 0;
+                        }
+                    } else {
+                        // Stop at end
+                        if (currentStop != stops.Length - 1) {
+                            currentStop++;
+                        } else {
+                            endWalkingSequence = true;
+                        }
+                    }
+                    // Set stop counter to next stop
+                    waitTimeDuringStopsCounter = waitTimeDuringStops[currentStop];
+                    // Set NPC walking
+                    isWalking = true;
+
                 }
-                // Set stop counter to next stop
-                waitTimeDuringStopsCounter = waitTimeDuringStops[currentStop];
-                // Set NPC walking
-                isWalking = true;
-            }
-        // Check if walking
-        } else {
-            // Move to stop
-            transform.position = Vector3.MoveTowards(transform.position, stops[currentStop], NPCMoveSpeed * Time.deltaTime);
-            // Check if at stop
-            if (transform.position == stops[currentStop]) {
-                // Set NPC not walking
-                isWalking = false;
+                // Check if walking
+            } else {
+                // Move to stop
+                transform.position = Vector3.MoveTowards(transform.position, stops[currentStop], NPCMoveSpeed * Time.deltaTime);
+                // Check if at stop
+                if (transform.position == stops[currentStop]) {
+                    // Set NPC not walking
+                    isWalking = false;
+                }
             }
         }
     }
